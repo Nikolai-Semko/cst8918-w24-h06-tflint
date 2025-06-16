bbc4321014d6a15a6e2c74ecf313a13d1551bbb7
NikolaiSemko <semk0001@algonquinlive.com>
2025-06-15 20:18:08 -0400

    install tflint, change from recommended to all

--- test/azure_webserver_test.go
+++ test/azure_webserver_test.go
@@ -10,7 +10,7 @@ import (
 
 // You normally want to run this under a separate "Testing" subscription
 // For lab purposes you will use your assigned subscription under the Cloud Dev/Ops program tenant
-var subscriptionID string = "<your-subscription-id>"
+var subscriptionID string = "100148d9-6e2a-4efd-b476-267627a09dfa"
 
 func TestAzureLinuxVMCreation(t *testing.T) {
 	terraformOptions := &terraform.Options{
@@ -18,7 +18,7 @@ func TestAzureLinuxVMCreation(t *testing.T) {
 		TerraformDir: "../",
 		// Override the default terraform variables
 		Vars: map[string]interface{}{
-			"labelPrefix": "<your-college-username>",
+			"labelPrefix": "semk0001",
 		},
 	}
 
